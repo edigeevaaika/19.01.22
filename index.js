3e7808b7e83dae9eaedd6c26b642151073094d44
Aida <edigeeva.aika@mail.ru>
2023-01-19 16:20:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+let myName = "Aida";
+
+console.log(myName);
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+console.log(myName.length);
+
+let pi = 3.141495;
+
+console.log(pi);
+console.log(pi.toFixed(2));
+console.log(pi.toFixed(0));
+
+let example1 = 2 + 2;
+let example2 = 2 / 2;
+let example3 = 2 * 2;
+let example4 = 2 - 2;
+
+let example5 = 2**4;
+let example6 = 81**(1/2);
+let example7 = 27 **(1/3);
+// console.log(example7); 
+
+let example8 = 5%2;
+// console.log(example8);
+
+let example9 = 41-7*3;
+
+let example10 = 5*2+3*4;
+
+let example11 = "5"+5;
+let example12 = "5"/5;
+let example13 = "a"/"b";
+
+let example14 = "Hello" +"World";
+
+let height = 8;
+let width = 5;
+let ploshad = height * width / 2;
+console.log(ploshad);
+
+let minute = 60;
+let hours = 56;
+let minutes = minute * hours;
+console.log(minutes);
+
+let day = 8;
+let hour = 8;
+let pay = 50;
+let money = (8*50)*8;
+console.log(money);
+
+let procent = 15;
+let check = 1566;
+let tips = check * procent / 100;
+console.log(tips);
+
+let gols = 8*2;
+let point = 6*3;
+let points = gols + point;
+console.log(points);
+
+let litr = 100;
+let kilometr = 100;
+let tractor = litr * kilometr / 22;
+console.log (tractor);
+
+
+let lightSwitchOn = false;
+let windowIsOpen = false;
+
+let example20 = 5 > 10;
+
+let example = 5 >= 10;
+let example16 =  5 <= 10;
+let example17 =  5 === 10;
+let example18 =  5 !== 10;
+console.log(example18);
+
+if (lightSwitchOn) {
+  console.log("Switch is on");
+}
+else {
+  console.log("Switch is off");
+}
+
+let a = 10;
+let b = 8;
+if (a === b) {
+  console.log("A equals B");
+} 
+else {
+  console.log("A is not equal to B");
+}
+
+let students = 25;
+let capacity = 20;
+if (students <= capacity) {
+  console.log("students fit");
+}
+else {
+ console.log("students don't fit");
+}
+console.log("Столица КР");
+let answer = "Бишкек";
+if ( answer === "Бишкек") {
+  console.log("Правильно");
+}
+else {
+  console.log("Не правильно");
+}
+let login = "elonMusk";
+let password = "I'M the best ";
+if (login === "ElonMusk" && password === "I'm the best"){
+  console.log("Correct");
+}
+else {
+  console.log("Incorrect");
+}
